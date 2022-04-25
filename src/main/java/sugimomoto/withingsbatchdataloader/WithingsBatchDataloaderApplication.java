b7c4691b85a6d1d161a981312b604f2e576d8723
sugimomoto <sugimomoto@gmail.com>
2022-04-26 08:01:01 +0900

    Initial commit

---
+++ src/main/java/sugimomoto/withingsbatchdataloader/WithingsBatchDataloaderApplication.java
@@ -0,0 +1,13 @@
+package sugimomoto.withingsbatchdataloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithingsBatchDataloaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WithingsBatchDataloaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sugimomoto/withingsbatchdataloader/WithingsBatchDataloaderApplicationTests.java
@@ -0,0 +1,13 @@
+package sugimomoto.withingsbatchdataloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WithingsBatchDataloaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
